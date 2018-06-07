f389f3ff536948bfda2884499d66ae94b6a04b17
nuclear0wl <litypony@gmail.com>
2018-06-07 10:07:20 -0400

    Unit tests in pipeline

--- jenkins/CI_job.groovy
+++ jenkins/CI_job.groovy
@@ -1,9 +1,20 @@
 node {
-    stage('Hello') {
-	sh "echo Hello"
+    stage('Initialize') {
+	def dockerHome = tool 'myDocker'
+        env.PATH = "${dockerHome}/bin:${env.PATH}"
     }
 
-    stage('World') {
-        sh "echo World"
+    stage('Checkout') {
+        deleteDir()
+        checkout scm
+    }
+
+    stage('Unit test') {
+	status = sh(returnStatus: true, script: "python3 -m unittest greetings_app/test_selects.py").trim()
+	if (status != 0) {
+	    currentBuild.result = 'FAILED'
+	    sh "echo Unit test was failed"
+	    sh "exit ${status}"
+	}
     }
 }
