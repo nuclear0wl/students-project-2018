074c22d4dd6766f4a5433c84146986684f4d1ba5
nuclear0wl <litypony@gmail.com>
2018-06-08 17:02:58 -0400

    Fixed dockerfile. Made compose-yml to use app image from hub. Added dockerignore to ignore database as a context. Made temlate for CD job. Trigger of CI_job will be tested by that commit.

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM python:3.7.0b4
 
 ENV DB_URL sqlite:///foo.db
-ADD greetings_app/* /greetings_app/
+ADD greetings_app/ /greetings_app/
 RUN pip install -r greetings_app/requirements.txt
 
 ENTRYPOINT python3 greetings_app/app.py
---
+++ jenkins/CI_CD_Seed.groovy
@@ -0,0 +1,45 @@
+def gitUrl = 'https://github.com/nuclear0wl/students-project-2018.git'
+
+pipelineJob("CI_job") {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url(gitUrl)
+			refspec('+refs/tags/*:refs/remotes/origin/tags/*')
+                        credentials('nuclear0wl-github')
+                    }
+                    branch('master/tags/*.*')
+                }
+            }
+            scriptPath("jenkins/CI_job.groovy")
+        }
+    }
+
+    triggers {
+	scm('*/10 * * * *')
+    }
+}
+
+pipelineJob("CD_job") {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url(gitUrl)
+			refspec('+refs/tags/*:refs/remotes/origin/tags/*')
+                        credentials('nuclear0wl-github')
+                    }
+                    branch('master/tags/*.*')
+                }
+            }
+            scriptPath("jenkins/CD_job.groovy")
+        }
+    }
+
+    triggers {
+	upstream('CI_job')
+    }
+}
