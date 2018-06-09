a432b4c6ad4dbbf04e885c5e9e3bf2e9e5013269
nuclear0wl <litypony@gmail.com>
2018-06-09 15:02:51 -0400

    Working on CD_job CD job was parameterized CI job push latest

--- jenkins/CI_job.groovy
+++ jenkins/CI_job.groovy
@@ -1,4 +1,5 @@
 def CONTAINER_NAME = "greetings_app"
+def HUB_USER = "nuclear0wl"
 
 node {
     stage('Initialize') {
@@ -13,26 +14,27 @@ node {
 
     stage('Build') {
 	TAG = sh(returnStdout: true, script: "git tag --contains").trim()
-	sh "docker build -t $CONTAINER_NAME:$TAG --pull --no-cache ."
+	sh "docker build -t $HUB_USER/$CONTAINER_NAME:$TAG -t $HUB_USER/$CONTAINER_NAME --pull --no-cache ."
 	echo "Image $CONTAINER_NAME:$TAG was builded successfully"
     }
 
     stage('Unit test') {
-	status = sh(returnStdout:true, script: "docker run --rm --entrypoint bash $CONTAINER_NAME:$TAG -c 'pip -q install mock && python3 greetings_app/test_selects.py 2> dev/null && echo \$?'").trim()
+	status = sh(returnStdout:true, script: "docker run --rm --entrypoint bash $HUB_USER/$CONTAINER_NAME:$TAG -c 'pip -q install mock && python3 greetings_app/test_selects.py 2> dev/null && echo \$?'").trim()
 	if (status != "0") {
 	    echo "STATUS ${status}"
 	    currentBuild.result = 'FAILED'
 	    echo "Unit tests were failed"
 	    sh "exit ${status}"
+	} else {
+	    echo "Unit tests were passed"
 	}
-	echo "Unit tests were passed"
     }
 
     stage('Push to Docker Registry') {
         withCredentials([usernamePassword(credentialsId: 'nuclear0wl-dockerhub', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
             sh "docker login -u $USERNAME -p $PASSWORD"
-            sh "docker tag $CONTAINER_NAME:$TAG $USERNAME/$CONTAINER_NAME:$TAG"
             sh "docker push $USERNAME/$CONTAINER_NAME:$TAG"
+	    sh "docker push $USERNAME/$CONTAINER_NAME"
             echo "Image push complete"
         }
     }
