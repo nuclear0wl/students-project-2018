039cfcba594043ec91891b1b66cc9e0e3bcd952f
nuclear0wl <litypony@gmail.com>
2018-06-10 15:54:59 -0400

    First version of integration tests

--- jenkins/CD_job.groovy
+++ jenkins/CD_job.groovy
@@ -10,8 +10,11 @@ node {
     }
 
     stage('Stop old container') {
-        sh "docker stop $CONTAINER_NAME && docker rm $CONTAINER_NAME"
-        echo "Old version of app was removed"
+	try {
+	    sh "docker stop $CONTAINER_NAME && docker rm $CONTAINER_NAME"
+	    echo "Old version of app was removed"
+	} catch (error) { 
+	}
     }
 
     stage('Run new container') {
@@ -30,9 +33,28 @@ node {
 	} else {
 	    echo "Container is running"
 	}
+
+	APP_ADDR = sh(returnStdout: true, script: "docker inspect $CONTAINER_NAME --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}'").trim()
+	status = sh(returnSatus: true, script: "curl --silent --connect-timeout 15 --show-error --fail http://$APP_ADDR:$APP_PORT")
+        if (status != 0) {
+	    echo "Can't connect to app"
+	    currentBuild.result = 'FAILED'
+	    sh "exit 2"
+	} else {
+	    echo "$APP_ADDR:$APP_PORT avaliable"
+	}
+
+	output = sh(returnStdout: true, script: "curl --silent --connect-timeout 15 --show-error --fail http://$APP_ADDR:$APP_PORT")
+	if (output.indexOf('Greetings, stranger!') < 0) {
+	    echo "Unexpected context of app"
+	    currentBuild.result = 'FAILED'
+	    sh "exit 3"
+	} else {
+	    echo "Context of app was approved"
+	}
     }
 
-    stage('Remove old version') {
+    stage('Mark new version as stable') {
 	sh "docker tag $HUB_USER/$IMAGE_NAME:${env.PARAM_GIT} $HUB_USER/$IMAGE_NAME:stable"
 	withCredentials([usernamePassword(credentialsId: 'nuclear0wl-dockerhub', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
             sh "docker login -u $USERNAME -p $PASSWORD"
