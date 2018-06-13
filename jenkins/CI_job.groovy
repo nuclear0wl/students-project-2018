d267fedb691f03abdda541566dbd0bafa370d69a
nuclear0wl <litypony@gmail.com>
2018-06-13 17:38:51 -0400

    Solved problem with docker tool Fixed small disadvantages in scripts

--- jenkins/CI_job.groovy
+++ jenkins/CI_job.groovy
@@ -3,8 +3,10 @@ def HUB_USER = "nuclear0wl"
 
 node {
     stage('Initialize') {
-	def dockerHome = tool 'myDocker'
-        env.PATH = "${dockerHome}/bin:${env.PATH}"
+        if (sh(returnStatus: true, script: "which docker")) {
+            def dockerHome = tool 'myDocker'
+            env.PATH = "${dockerHome}/bin:${env.PATH}"
+        }
     }
 
     stage('Checkout') {
@@ -13,28 +15,28 @@ node {
     }
 
     stage('Build') {
-	TAG = sh(returnStdout: true, script: "git tag --contains").trim()
-	sh "docker build -t $HUB_USER/$CONTAINER_NAME:$TAG -t $HUB_USER/$CONTAINER_NAME --pull --no-cache ."
-	echo "Image $CONTAINER_NAME:$TAG was builded successfully"
+        TAG = sh(returnStdout: true, script: "git tag --contains").trim()
+        sh "docker build -t $HUB_USER/$CONTAINER_NAME:$TAG -t $HUB_USER/$CONTAINER_NAME --pull --no-cache ."
+        echo "Image $CONTAINER_NAME:$TAG was builded successfully"
     }
 
     stage('Unit test') {
-	status = sh(returnStdout:true, script: "docker run --rm --entrypoint bash $HUB_USER/$CONTAINER_NAME:$TAG -c 'pip -q install mock && python3 greetings_app/test_selects.py 2> dev/null && echo \$?'").trim()
-	if (status != "0") {
-	    echo "STATUS ${status}"
-	    currentBuild.result = 'FAILED'
-	    echo "Unit tests were failed"
-	    sh "exit ${status}"
-	} else {
-	    echo "Unit tests were passed"
-	}
+        status = sh(returnStdout:true, script: "docker run --rm --entrypoint bash $HUB_USER/$CONTAINER_NAME:$TAG -c 'pip -q install mock && python3 greetings_app/test_selects.py 2> dev/null && echo \$?'").trim()
+        if (status != "0") {
+            echo "STATUS ${status}"
+            currentBuild.result = 'FAILED'
+            echo "Unit tests were failed"
+            sh "exit ${status}"
+        } else {
+            echo "Unit tests were passed"
+        }
     }
 
     stage('Push to Docker Registry') {
         withCredentials([usernamePassword(credentialsId: 'nuclear0wl-dockerhub', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
             sh "docker login -u $USERNAME -p $PASSWORD"
             sh "docker push $USERNAME/$CONTAINER_NAME:$TAG"
-	    sh "docker push $USERNAME/$CONTAINER_NAME"
+            sh "docker push $USERNAME/$CONTAINER_NAME"
             echo "Image push complete"
         }
     }
